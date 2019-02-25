beaa90e7fd1ce21080736cb9e497396302f2345a
andy reyes <11710reyes@gmail.com>
2019-02-25 22:08:40 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113163910) do
+ActiveRecord::Schema.define(version: 20190225220729) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,9 +30,11 @@ ActiveRecord::Schema.define(version: 20160113163910) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "category_id"
+    t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
